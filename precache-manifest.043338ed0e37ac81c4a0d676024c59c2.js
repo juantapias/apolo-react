b0592fb7a3df9e528a952b9c7bed4ae19e97d947
Juan Tapias <juantapias@Juan-Tapias.local>
2020-09-23 12:49:57 -0500

    Updates

---
+++ precache-manifest.043338ed0e37ac81c4a0d676024c59c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6931049002e57e3acb9dc3b5c91952c",
+    "url": "/apolo-react/index.html"
+  },
+  {
+    "revision": "0b1023b571bfb0cd3218",
+    "url": "/apolo-react/static/css/main.caa2e0cc.chunk.css"
+  },
+  {
+    "revision": "bb354223ff7da1d69a74",
+    "url": "/apolo-react/static/js/2.3c0bb42f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/apolo-react/static/js/2.3c0bb42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1023b571bfb0cd3218",
+    "url": "/apolo-react/static/js/main.9fb5023c.chunk.js"
+  },
+  {
+    "revision": "42eb4c81f387f100b087",
+    "url": "/apolo-react/static/js/runtime-main.e12b8f01.js"
+  },
+  {
+    "revision": "45272c6266a9c9581d5c140b4bb540d0",
+    "url": "/apolo-react/static/media/contact-img.45272c62.png"
+  },
+  {
+    "revision": "36d563d8a0512f3bc79cb458375a0f9d",
+    "url": "/apolo-react/static/media/left-asset.36d563d8.png"
+  }
+]);
